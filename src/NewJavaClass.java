7f2a3424a9050d07701eaaf6061875602cd92789
SlimeBlaster <viktor_veli@abv.bg>
2022-07-05 12:01:05 +0300

    Initial commit.

--- src/NewJavaClass.java
+++ src/NewJavaClass.java
@@ -1,4 +1,23 @@
 public class NewJavaClass {
+public static void main(String[] args){
+    System.out.print("" + makeSomething());
 
-    int x = 0;
+    byte i = 1;
+    byte j = 2;
+    byte sum = (byte) (i+j);
+    System.out.print(", " + sum);
+
+    char c = 'a';
+}
+public static int makeSomething(int i, int j){
+    return i+j;
+}
+public static String makeSomething(String name){
+    return "Hello" + name;
+}
+public static double makeSomething(){
+    return 3.14;
+}
+
+int x = 0;
 }
