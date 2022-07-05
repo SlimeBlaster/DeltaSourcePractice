aff7bb8bbd6020047579dfefa50cc73d84b94e60
SlimeBlaster <108656992+SlimeBlaster@users.noreply.github.com>
2022-07-06 01:55:59 +0300

    Update and rename Homework.java to DollarInLeva.java

--- src/DollarInLeva.java
+++ src/DollarInLeva.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-public class Homework{
+public class DollarInLeva{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int dollar = s.nextInt();
         double leva = 1.64549;
         double levaInDollar = leva * dollar;
-
         System.out.print((Math.floor(levaInDollar * 100)) / 100);
     }
 }
