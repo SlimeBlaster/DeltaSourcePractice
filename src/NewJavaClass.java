766f64b380befb393518d60abcf5165b82a41dcf
SlimeBlaster <viktor_veli@abv.bg>
2022-07-05 13:12:46 +0300

    Initial commit.

--- src/NewJavaClass.java
+++ src/NewJavaClass.java
@@ -1,23 +1,11 @@
+import java.util.Scanner;
 public class NewJavaClass {
-public static void main(String[] args){
-    System.out.print("" + makeSomething());
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int dollar = s.nextInt();
+        double leva = 1.64549;
+        double levaInDollar = leva * dollar;
 
-    byte i = 1;
-    byte j = 2;
-    byte sum = (byte) (i+j);
-    System.out.print(", " + sum);
-
-    char c = 'a';
-}
-public static int makeSomething(int i, int j){
-    return i+j;
-}
-public static String makeSomething(String name){
-    return "Hello" + name;
-}
-public static double makeSomething(){
-    return 3.14;
-}
-
-int x = 0;
+        System.out.print((Math.floor(levaInDollar * 100)) / 100);
+    }
 }
