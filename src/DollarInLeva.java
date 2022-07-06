02b96e31dda513aff1f0960fbae9cfe87dd0a060
SlimeBlaster <108656992+SlimeBlaster@users.noreply.github.com>
2022-07-06 18:08:23 +0300

    Update DollarInLeva.java

--- src/DollarInLeva.java
+++ src/DollarInLeva.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class DollarInLeva{
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        System.out.print("Write how many dollars to be converted in leva: ")
         int dollar = s.nextInt();
         double leva = 1.64549;
         double DollarsInLeva = leva * dollar;
